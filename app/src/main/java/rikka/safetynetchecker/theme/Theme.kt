efbdefdaad189c5dc939faf0285d4734205a797c
RikkaW <rikka@shizuku.moe>
2021-09-24 15:23:08 +0800

    Use system colors on Android 12+

--- app/src/main/java/rikka/safetynetchecker/theme/Theme.kt
+++ app/src/main/java/rikka/safetynetchecker/theme/Theme.kt
@@ -1,38 +1,57 @@
 package rikka.safetynetchecker.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+@Composable
+private fun darkColorPalette() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    darkColors(
+        primary = colorResource(android.R.color.system_accent1_100),
+        primaryVariant = colorResource(android.R.color.system_accent1_200),
+        secondary = colorResource(android.R.color.system_accent1_300),
+        secondaryVariant = colorResource(android.R.color.system_accent1_300),
+        surface = colorResource(android.R.color.system_neutral1_900),
+        background = colorResource(android.R.color.system_neutral1_900),
+    )
+} else {
+    darkColors(
+        primary = Purple200,
+        primaryVariant = Purple700,
+        secondary = Teal200,
+        secondaryVariant = Teal200
+    )
+}
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+@Composable
+private fun lightColorPalette() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    lightColors(
+        primary = colorResource(android.R.color.system_accent1_500),
+        primaryVariant = colorResource(android.R.color.system_accent1_500),
+        secondary = colorResource(android.R.color.system_accent1_600),
+        secondaryVariant = colorResource(android.R.color.system_accent1_600),
+        surface = colorResource(android.R.color.system_neutral1_50),
+        background = colorResource(android.R.color.system_neutral1_50),
+    )
+} else {
+    lightColors(
+        primary = Purple500,
+        primaryVariant = Purple700,
+        secondary = Teal200,
+        secondaryVariant = Teal200,
+    )
+}
 
 @Composable
 fun YetAnotherSafetyNetCheckerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        darkColorPalette()
     } else {
-        LightColorPalette
+        lightColorPalette()
     }
 
     MaterialTheme(
